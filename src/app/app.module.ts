a9db695b4b10f31043f73bd64cb430185f294ca5
MaryamTalal <maryamtselim@gmail.com>
2020-03-25 03:05:46 +0200

    home

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FontAwesomeModule } from "../../node_modules/@fortawesome/angular-fontawesome";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { AccountPeakComponent } from "./home-page/account-peak/account-peak.component";
+import { HomeConnectorsComponent } from "./home-page/home-connectors/home-connectors.component";
+import { HomeConnectorsCardsComponent } from "./home-page/home-connectors/home-connectors-cards/home-connectors-cards.component";
+import { PostsComponent } from "./home-page/posts/posts.component";
+import { TagsComponent } from "./home-page/tags/tags.component";
+import { PostCardsComponent } from "./home-page/posts/post-cards/post-cards.component";
+import { HomePageHeaderComponent } from './homepageheader/homepageheader.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    AccountPeakComponent,
+    HomeConnectorsComponent,
+    HomeConnectorsCardsComponent,
+    PostsComponent,
+    TagsComponent,
+    PostCardsComponent,
+    HomePageHeaderComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
